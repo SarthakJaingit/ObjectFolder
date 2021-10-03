99afda61d0d5a1a4e7290acbdcadb13e9cf95daa
rhgao <rhgao@cs.stanford.edu>
2021-10-03 10:44:43 -0700

    reorg

--- AudioNet/test_script.sh
+++
@@ -1,8 +0,0 @@
-python evaluate.py \
---vertex_file_path /viscam/u/rhgao/datasets/ObjectFiles/YCB/ycb/030_fork/google_64k/vertices.npy \
---modes_file_path /viscam/u/rhgao/datasets/ObjectFiles/YCB/ycb/030_fork/google_64k/modes.npy \
---model_path ./checkpoints/exp2/model.pt \
---gpu_ids 0,1 \
---batchSize 10000 \
---network_depth 8 \
---results_dir ./results/exp2/
